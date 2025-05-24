1ad301354def1052c30c25ff46ae3694b0d13000
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2025-05-24 13:42:34 +0530

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,1 +1,12 @@
-# Currently no helpers needed, keep it for future utility functions
+from pymongo import MongoClient
+import os
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["telemingle"]
+users = db["users"]
+
+def is_registered(user_id):
+    return users.find_one({"user_id": user_id})
+
+def register_user(user_id, gender, name):
+    users.insert_one({"user_id": user_id, "gender": gender, "name": name})
