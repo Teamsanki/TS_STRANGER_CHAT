fbebcb951b50cf9bf444ee518fe952d77a2b21ba
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2025-05-24 14:16:22 +0530

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,23 @@
 from pymongo import MongoClient
 import os
 
+# Connect to MongoDB using the MONGO_URI environment variable
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client["telemingle"]
 users = db["users"]
 
 def is_registered(user_id):
-    return users.find_one({"user_id": user_id})
+    """Check if a user is already registered."""
+    return users.find_one({"user_id": user_id}) is not None
 
 def register_user(user_id, gender, name):
-    users.insert_one({"user_id": user_id, "gender": gender, "name": name})
+    """Register a new user."""
+    users.insert_one({
+        "user_id": user_id,
+        "gender": gender,
+        "name": name
+    })
+
+def get_user_data(user_id):
+    """Get a registered user's data (name and gender)."""
+    return users.find_one({"user_id": user_id}) or {}
